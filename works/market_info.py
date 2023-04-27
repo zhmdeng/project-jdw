8f83b3e99b2d23e1d5073bf15a7ee21cd006d482
zhmdeng <zhimdeng@163.com>
2023-04-27 11:13:30 +0800

    refresh

--- works/market_info.py
+++ works/market_info.py
@@ -1,6 +1,3 @@
-
-
-
 from utils.combine import Combine
 from bases.logs import Logs
 from bases.base import Base
@@ -8,7 +5,6 @@ from bases.date import Date
 from bases.config import Config
 from utils.trans_files import Transfer
 from utils.generate import Generate
-
 
 if __name__ == '__main__':
     log_file = 'market_info'
@@ -18,62 +14,62 @@ if __name__ == '__main__':
     config = Config()
     generate = Generate()
     try:
-            # read path
-            path = r"E:\marketing"
-            # output file name
-            final_name = "\market_info"
-            # output path
-            path_ = r"E:\orders\combine"
+        # read path
+        path = r"E:\marketing"
+        # output file name
+        final_name = "\market_info"
+        # output path
+        path_ = r"E:\orders\combine"
 
-            # combine files
-            combine = Combine()
-            final_data = combine.get_all_files(path,log_file)
-            logs.info("", f"files combine successful ...........", log_file)
-            print(final_data.columns)
-            # data cleaning
-            final_data = final_data[['子行业', '父行业类目名', '平台', '日期', '交易金额', '父行业交易金额', '支付子订单数较父行业占比']]
+        # combine files
+        combine = Combine()
+        final_data = combine.get_all_files(path, log_file)
+        logs.info("", f"files combine successful ...........", log_file)
+        print(final_data.columns)
+        # data cleaning
+        final_data = final_data[['子行业', '父行业类目名', '平台', '日期', '交易金额', '父行业交易金额', '支付子订单数较父行业占比']]
 
-            final_data.columns = ['sector','upper_sector','plat','created','gross_merchandise_volume','upper_money','pay_ratio']
+        final_data.columns = ['sector', 'upper_sector', 'plat', 'created', 'gross_merchandise_volume', 'upper_money'
+            , 'pay_ratio']
 
-            final_data = final_data.reset_index(drop=True)
-            for i in range(len(final_data)):
-                final_data['gross_merchandise_volume'][i] = str(final_data['gross_merchandise_volume'][i]).replace(",", "")
-                final_data['upper_money'][i] = str(final_data['upper_money'][i]).replace(",","")
-                final_data['pay_ratio'][i] = str(final_data['pay_ratio'][i]).replace("%", "")
+        final_data = final_data.reset_index(drop=True)
+        for i in range(len(final_data)):
+            final_data['gross_merchandise_volume'][i] = str(final_data['gross_merchandise_volume'][i]).replace(",", "")
+            final_data['upper_money'][i] = str(final_data['upper_money'][i]).replace(",", "")
+            final_data['pay_ratio'][i] = str(final_data['pay_ratio'][i]).replace("%", "")
 
-            values = ['unknow','unknow','unknow','0000-00-00',-1.00,-1.00,-1.00]
-            base.fillna_col(final_data, final_data.columns, values, log_file)
+        values = ['unknow', 'unknow', 'unknow', '0000-00-00', -1.00, -1.00, -1.00]
+        base.fillna_col(final_data, final_data.columns, values, log_file)
 
-            new_type = ['str','str','str','datetime64','float','float','float']
-            base.retype(final_data, final_data.columns, new_type, log_file)
+        new_type = ['str', 'str', 'str', 'datetime64', 'float', 'float', 'float']
+        base.retype(final_data, final_data.columns, new_type, log_file)
 
-            final_data['load_date'] = date.current_time()
-            final_data['update_date'] = date.current_time()
-            print(len(final_data))
-            final_data.drop_duplicates(['sector', 'upper_sector', 'plat', 'created', 'gross_merchandise_volume',
-                                        'upper_money', 'pay_ratio', 'load_date', 'update_date'],inplace=True)
-            print(len(final_data))
-            # save to csv
-            time = str(max(final_data['created'])).split(" ")[0]
-            final_data.to_csv(path_ + final_name + f"{time}" + ".csv",index=False)
-            logs.info("", "data load to excel successful ...........", log_file)
+        final_data['load_date'] = date.current_time()
+        final_data['update_date'] = date.current_time()
+        print(len(final_data))
+        final_data.drop_duplicates(['sector', 'upper_sector', 'plat', 'created', 'gross_merchandise_volume',
+                                    'upper_money', 'pay_ratio', 'load_date', 'update_date'], inplace=True)
+        print(len(final_data))
+        # save to csv
+        time = str(max(final_data['created'])).split(" ")[0]
+        final_data.to_csv(path_ + final_name + f"{time}" + ".csv", index=False)
+        logs.info("", "data load to excel successful ...........", log_file)
 
-            # # save to database
-            # db_info = config.get_parameters('wms')
-            # con = base.get_connection(db_info)
-            #
-            # base.save_sql(final_data,"market_info",con  ,"wms",log_file)
-            # logs.critical("", "data load to database successfully ...........\n", log_file)
-            #
-            # move_path = r"E:\市场"
-            # path = r"E:\marketing\\"
-            # transfer = Transfer()
-            # transfer.move_file(path,move_path, log_file)
-            #
-            # generate.generate_folder(path,log_file)
-            #
-            #
-            # logs.info("", "Job run successful ...........\n", log_file)
+        # # save to database
+        # db_info = config.get_parameters('wms')
+        # con = base.get_connection(db_info)
+        #
+        # base.save_sql(final_data ,"market_info" ,con ,"wms" ,log_file)
+        # logs.critical("", "data load to database successfully ...........\n", log_file)
+        #
+        # move_path = r"E:\市场"
+        # path = r"E:\marketing"
+        # transfer = Transfer()
+        # transfer.move_file(path ,move_path, log_file)
+        #
+        # generate.generate_folder(path ,log_file)
+
+        logs.info("", "Job run successful ...........\n", log_file)
 
     except Exception as e:
         print(e)
--- works/test2.py
+++ works/test2.py
@@ -1,32 +1,27 @@
 
 
-import jieba
-from jieba.analyse import extract_tags
 
-
-
-# 导入相关库
 import pandas as pd
 import jieba
 
-# 读取数据
+
 data = pd.read_excel(r'E:\评论\店透视_评论分析_100005527962.xlsx')
 
-# 查看数据
+
 data.head()
-# 生成分词
+
 data['cut'] = data['首次评价'].apply(lambda x : list(jieba.cut(x)))
 
 print(data.head())
 
-# 读取停用词数据
+
 stopwords = pd.read_csv('StopwordsCN.txt', encoding='utf8', names=['stopword'], index_col=False)
 
 print(stopwords.head())
-# 转化词列表
+
 stop_list = stopwords['stopword'].tolist()
 
-# 去除停用词
+
 data['cut'] = data['首次评价'].apply(lambda x : [i for i in jieba.cut(x) if i not in stop_list])
 
 print(data.head(10))
@@ -41,7 +36,7 @@ for content in data['cut']:
 corpus = pd.DataFrame(words, columns=['word'])
 corpus['cnt'] = 1
 
-# 分组统计
+
 g = corpus.groupby(['word']).agg({'cnt': 'count'}).sort_values('cnt', ascending=False)
 
 print(g.head(70))
